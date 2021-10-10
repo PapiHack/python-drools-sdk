1a8e0f4b640c044aa5c9ebb68f5261c6c6745e79
PapiHack <mssmbaye@gmail.com>
2021-10-10 21:42:57 +0000

    Edit package_dir option in setup.py file

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    package_dir={"": "src"},
+    package_dir={"python_drools_sdk": "src"},
     packages=setuptools.find_packages(where="src"),
     python_requires=">=3.6",
 )
